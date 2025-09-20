e5184bca7ba9891f964982cd81c34f715e339377
Nathaniel <nsifuent_13@berkeley.edu>
2025-09-19 17:23:20 -0700

    adding files

---
+++ homework2/homework2.py
@@ -0,0 +1,77 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+	# Git is a version control system that you can run locally on your 
+	# own  computer
+# 2) What’s the difference between the terminal and the command line?
+	# The terminal is the program that lets you interact with the shell 
+	# you are working within. The command line is the text-based
+	# interface inside the terminal where you type your commands.
+# 3) How does Windows PowerShell differ from Git Bash?
+	# Windows Powershell is the type of shell that Windows uses by
+	# default. Git Bash is a different kind of shell that is more
+	# unix-like and works better with Git commands.
+# 4) What’s the difference between Anaconda, conda, and Python?
+	# Anaconda is a distribution that bundles many different programs and
+	# libraries together in one place. Conda is package and environment
+	# manager that handles the changes to these packages or environments.
+	# Python is the programming language itself that is bundled within
+	# Anaconda.
+# 5) What is VS Code? 
+	# VS Code (Visual Studio Code) is a scipting program, that is highly 
+	# customizable, that you can use to help debug your code.
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+	# Juypter Notebook is an environment that you can use to write up 
+	# and test your code. Juypter Lab is similar but it has more features
+	# like allowing you to access a terminal, multiple tabs, etc.
+# 7) What does ~/ mean?
+	# ~/ is a shorthand for your home (or root) directory.
+# 8) What’s the difference between an absolute path and a relative path?
+	# An absolute path is the full path to your current working directory
+	# starting from your root or home directory. A relative path is one 
+	# that does not start from your root directory but still tells you
+	# what your current working directory is.
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+	# An absolute path would be: C:\Users\username\python_decal_fa25\course_assignments\homework2 
+	# (I used \ rather than / becasue that is what is used for windows)
+	# A relative path would be: course_assignments\homework2
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+	# cd ..
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+	# This essentially says to remove (rm) everything inside 
+	# your current working directory (./)
+# 12) What do the following commands do?
+# git add: This command adds whatever you specify to the staging area.
+# git commit: This command commits those things in the staging area to be sent to the remote repo.
+# git push: This command pushes the commits to the remote repo, essentialluy saving them there.
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+	# git add . is what you use to add all files in the directory 
+	# to the staging area, whereas git add <file> only adds the
+	# file you specify to the staging area.
+# 14) What do "git status" and "git log -1" do?
+	# git status pulls up the status of your local repo that details
+	# what you have added, what files are untracked, and more. 
+	# git log -1 pulls up your log of recent (specifically the most
+	# recent one you used) git commits.
+# 15) What’s the difference between cloning a repository and pulling from it?
+	# Cloning a repository is when you save a copy of its contents to
+	# your local system, whereas pulling a repository is when you 
+	# update your local copy of the repository.
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+	# I had an issue where the incorrect folder was being pushed to the remote repo, and I got it fixed
+	# by asking on ed discussion about what I was doing wrong.
+# 17) What’s a question you still have? What’s something you’re confused about?
+	# How can I make a folder/file no longer be tracked by git. Something I am confused about is the 
+	# exact process by which git tracks which files are to be pushed the the remote repo or if files
+	# are being cached.
+# 18) Tell me a fun fact!
+	# In Super Smash Bros Ultimate, Luigi's down taunt can spike (launch downward) the enemy.
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+	# print(2 * 2) # multiples 2 by 2, it is my favorite because multiplication is exteremely useful
